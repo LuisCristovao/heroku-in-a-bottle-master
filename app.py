91645a6cce3ccb702e41a9f23036a53babe17aa1
LuisCristovao <luis.cristovao92@gmail.com>
2018-04-05 14:27:24 +0100

    simplification

--- app.py
+++ app.py
@@ -13,21 +13,8 @@ bottle.debug(True)
 @get('/')
 def index():
     response.content_type = 'text/plain; charset=utf-8'
-    ret =  'Hello world, I\'m %s!\n\n' % os.getpid()
-    ret += 'Request vars:\n'
-    for k, v in request.environ.iteritems():
-        if 'bottle.' in k:
-            continue
-        ret += '%s=%s\n' % (k, v)
+    
 
-    ret += '\n'
-    ret += 'Environment vars:\n'
-
-    for k, v in env.iteritems():
-        if 'bottle.' in k:
-            continue
-        ret += '%s=%s\n' % (k, v)
-
-    return ret
+    return "Finally first heroku app!"
 
 bottle.run(host='0.0.0.0', port=argv[1])
